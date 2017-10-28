a8e102a126c7281acba8b8a8db6b3fe441e3a39f
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2017-10-27 19:42:52 -0700

    Created a template file to use when creating new pages

---
+++ template.php
@@ -0,0 +1,16 @@
+<?php
+$title = ''; # Enter title of page
+$css_files = array(
+  # put css files here
+);
+include $_SERVER['DOCUMENT_ROOT'] . 'php/phtml/html_header.phtml';
+?>
+<!--
+  Page Contents go in here
+-->
+<?php
+$js_files = array(
+  # put js files here
+);
+include $_SERVER['DOCUMENT_ROOT'] . 'php/phtml/html_footer.phtml';
+?>
