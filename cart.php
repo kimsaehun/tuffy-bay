3a9d8c4253fd34bc818f18c3fcdd367825c516ce
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2017-11-01 20:47:09 -0700

    Reorganized elements

--- cart.php
+++ cart.php
@@ -11,18 +11,18 @@ include $_SERVER['DOCUMENT_ROOT'] . 'php/phtml/html_header.phtml';
 
 <form action="" method="post">
   <div class="item-card">
-    <input type="hidden" name="item-id-1" value="1">
     <span class="name">Item #1</span>
     <input type="number" name="quantity-1" min="1">
     <span class="base-price">9.00</span>
     <span class="price"></span>
+    <input type="hidden" name="item-id-1" value="1">
   </div>
   <div class="item-card">
-    <input type="hidden" name="item-id-2" value="2">
     <span class="name">Item #2</span>
     <input type="number" name="quantity-2" min="1">
     <span class="base-price">6.50</span>
     <span class="price"></span>
+    <input type="hidden" name="item-id-2" value="2">
   </div>
   <div id="purchase-btn">
     <button type="submit">Buy Now!</button>
