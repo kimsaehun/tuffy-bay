9c564a2aa4eeb4983d56b3e2b5f41ad9079fde8c
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2017-11-06 13:36:40 -0800

    Add account.php

---
+++ account.php
@@ -0,0 +1,129 @@
+<?php
+$title = 'Tuffy Bay';
+$css_files = array();
+include $_SERVER['DOCUMENT_ROOT'] . '/page_modules/html_header.php';
+?>
+
+<div class="container">
+	  <ol class="breadcrumb">
+		  <li><a href="index.html">Home</a></li>
+		  <li class="active">Account</li>
+		 </ol>
+	 <div class="registration">
+		 <div class="registration_left">
+			 <h2>new user? <span> create an account </span></h2>
+			 <!-- [if IE]
+				< link rel='stylesheet' type='text/css' href='ie.css'/>
+			 [endif] -->
+
+			 <!-- [if lt IE 7]>
+				< link rel='stylesheet' type='text/css' href='ie6.css'/>
+			 <! [endif] -->
+			 <script>
+				(function() {
+
+				// Create input element for testing
+				var inputs = document.createElement('input');
+
+				// Create the supports object
+				var supports = {};
+
+				supports.autofocus   = 'autofocus' in inputs;
+				supports.required    = 'required' in inputs;
+				supports.placeholder = 'placeholder' in inputs;
+
+				// Fallback for autofocus attribute
+				if(!supports.autofocus) {
+
+				}
+
+				// Fallback for required attribute
+				if(!supports.required) {
+
+				}
+
+				// Fallback for placeholder attribute
+				if(!supports.placeholder) {
+
+				}
+
+				// Change text inside send button on submit
+				var send = document.getElementById('register-submit');
+				if(send) {
+					send.onclick = function () {
+						this.innerHTML = '...Sending';
+					}
+				}
+
+			 })();
+			 </script>
+			 <div class="registration_form">
+			 <!-- Form -->
+				<form id="registration_form" action="contact.php" method="post">
+					<div>
+						<label>
+							<input placeholder="first name:" type="text" tabindex="1" required autofocus>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="last name:" type="text" tabindex="2" required autofocus>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="email address:" type="email" tabindex="3" required>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="Mobile:" type="email" tabindex="3" required>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="password" type="password" tabindex="4" required>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="retype password" type="password" tabindex="4" required>
+						</label>
+					</div>
+					<div>
+						<input type="submit" value="create an account" id="register-submit">
+					</div>
+				</form>
+				<!-- /Form -->
+			 </div>
+		 </div>
+		 <div class="registration_left">
+			 <h2>existing user</h2>
+			 <div class="registration_form">
+			 <!-- Form -->
+				<form id="registration_form" action="contact.php" method="post">
+					<div>
+						<label>
+							<input placeholder="email:" type="email" tabindex="3" required>
+						</label>
+					</div>
+					<div>
+						<label>
+							<input placeholder="password" type="password" tabindex="4" required>
+						</label>
+					</div>
+					<div>
+						<input type="submit" value="sign in" id="register-submit">
+					</div>
+				</form>
+			 <!-- /Form -->
+			 </div>
+		 </div>
+		 <div class="clearfix"></div>
+	 </div>
+</div>
+
+<?php
+$js_files = array();
+include $_SERVER['DOCUMENT_ROOT'] . '/page_modules/html_footer.php';
+?>
