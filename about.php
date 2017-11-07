4d46752aa830f5ef01de467ee6247e6d37c0655b
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2017-11-06 20:21:53 -0800

    Update about.php

--- about.php
+++ about.php
@@ -4,7 +4,7 @@ $css_files = array();
 include $_SERVER['DOCUMENT_ROOT'] . '/page_modules/html_header.php';
 ?>
 <div class="container">
-  <h1>About Us</h1>
+  <h1>Who We Are</h1>
   <p>TuffyBay is an electronic commerce website dedicated to selling novelty college supplies to the students, faculty members, and supporters of California State University, Fullerton. By partnering with Student Store, TuffyBay provides a quick and easy way for students and faculty members to purchase college necessities at their own leisure. TuffyBay will provide an attractive design geared towards students. Through TuffyBay, supporters of California State University, Fullerton will now be able to show their support from anywhere within the states.</p>
 </div>
 <?php
