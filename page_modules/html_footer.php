6829c5431f5d3e93a0661ea211e7112c388a14bf
Kim Sae Hun <kimsaehun@csu.fullerton.edu>
2017-11-06 20:29:39 -0800

    Updated footer

--- page_modules/html_footer.php
+++ page_modules/html_footer.php
@@ -1,4 +1,27 @@
-<div class="copywrite footer">
+<div class="footer">
+	 <div class="container">
+		 <div class="ftr-grids">
+			 <div class="col-md-4 ftr-grid">
+					 <h4>Sponsors</h4>
+					 <img src="/images/sponsors.png"/>
+			 </div>
+			 <div class="col-md-4 ftr-grid">
+					 <h4>Affiliate</h4>
+					 <img src="/images/CSU-Fullerton.png" width="100%"/>
+			 </div>
+			 <div class="col-md-4 ftr-grid text-right">
+				 <h4>About Us</h4>
+				 <ul>
+					 <li><a href="/about.php">Who We Are</a></li>
+					 <li><a href="/contact.php">Contact Us</a></li>
+					 <li><a href="/faq.php">Frequently Asked Questions</a></li>
+				 </ul>
+			 </div>
+			 <div class="clearfix"></div>
+		 </div>
+	 </div>
+</div>
+<div class="copywrite">
 	<div class="container">
 		<p>Copyright © 2017 TuffyBay. All Rights Reserved | Design by <a href="http://w3layouts.com">W3layouts</a> | Modified by Unintentional Success</p>
 	</div>
